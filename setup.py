8982a10d9e93c4afd968229c3a708fce4b60d323
Nabeel <seedatnabeel@gmail.com>
2020-07-05 16:09:12 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='covid_19_chatbot',
+   version='1.0',
+   description='Chatbot for Covid-19 that can answer questions & classify images of masks',
+   author='Nabeel Seedat',
+   author_email='seedatnabeel@gmail.com',
+   packages=['covid_19_chatbot'],  
+)
